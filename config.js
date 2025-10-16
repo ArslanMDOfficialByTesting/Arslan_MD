df4ec5f6bdc0be1e80d317ce1b5a24f9631947ba
ArslanMDOfficialByTesting <muhammadarslanmdofficial@gmail.com>
2025-10-16 10:52:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~6FkVXIyL#kefEBvX9BR81bhmXSUD3OLzWU0AQiBQXVJZ9wGr_eSA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
